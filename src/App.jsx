32a694f3a602dff0b3a330617cd9f1dc8f74fb8a
abelsanxd <abelsan4665@gmail.com>
2024-04-22 03:44:08 -0300

    se hizo un prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import React, { useState } from 'react'; // eslint-disable-next-line
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { useState } from "react"; // eslint-disable-next-line
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  
-  const [inputValue, setInputValue] = useState(''); // Estado para el valor del input
-  const [tasks, setTasks] = useState([]); // Estado para almacenar las tareas
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-  // Función para manejar el cambio en el input
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  // Función para manejar el envío del formulario (agregar tarea)
   const handleSubmit = (e) => {
-    e.preventDefault(); // Evitar que se recargue la página al enviar el formulario
-    if (inputValue.trim() !== '') {
-      setTasks([...tasks, inputValue]); // Agregar la nueva tarea al array de tareas
-      setInputValue(''); // Limpiar el input
+    e.preventDefault();
+    if (inputValue.trim() !== "") {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
     }
   };
 
@@ -33,13 +30,17 @@ function App() {
             className="form-control"
             placeholder="Ingrese una tarea..."
           />
-          <button type="submit" className="btn btn-primary">Agregar</button>
+          <button type="submit" className="btn btn-primary">
+            Agregar
+          </button>
         </div>
       </form>
       <ul className="list-group">
-        {/* Iterar sobre las tareas y mostrar cada una en un elemento de lista */}
+        {}
         {tasks.map((task, index) => (
-          <li key={index} className="list-group-item">{task}</li>
+          <li key={index} className="list-group-item">
+            {task}
+          </li>
         ))}
       </ul>
     </div>
